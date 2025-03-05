c00f5cfc679252380663eeb0f77d21ddbf3f9f0d
Buzonga <gbcosta@live.com>
2025-03-05 13:45:31 -0300

    added aliens movement

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
     def __init__(self, xi_game):
         super().__init__()
         self.screen = xi_game.screen
+        self.settings = xi_game.settings
 
         self.image = pg.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
@@ -14,3 +15,11 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
--- xenos_invasion.py
+++ xenos_invasion.py
@@ -34,8 +34,9 @@ class XenosInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()
             self._update_bullets()
+            self._update_aliens()
+            self._update_screen()
             self.clock.tick(60)
 
         # Hydra Dominatus!
@@ -96,6 +97,22 @@ class XenosInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
@@ -108,6 +125,7 @@ class XenosInvasion:
         for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
+
 
     def _update_screen(self):
          
